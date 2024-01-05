a4bb8b5337430311be4d49bd8fcaf74104c6544c
David Hermann <redeagle.private@gmail.com>
2024-01-06 00:53:30 +0100

    Initial Commit

---
+++ Counting/CountingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountingApp.swift
+//  Counting
+//
+//  Created by David Hermann on 06.01.24.
+//
+
+import SwiftUI
+
+@main
+struct CountingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
