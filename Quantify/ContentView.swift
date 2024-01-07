ac27898784cb70a6677f7b1b6b67d1e68db0b8be
David Hermann <redeagle.private@gmail.com>
2024-01-07 22:42:39 +0100

    Some stuff

--- Quantify/ContentView.swift
+++ Quantify/ContentView.swift
@@ -144,8 +144,6 @@ struct Kategorie: Identifiable, Codable {
         case id, name, anlegeDatum, counter
     }
     
-
-    
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(id, forKey: .id)
